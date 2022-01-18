edac61804f0c43bd1bb3118676714a1459d0342c
Ankit492000 <ankit04092000.ap@gmail.com>
2022-01-18 12:12:00 +0530

    My First Commit

--- public/js/script.js
+++ public/js/script.js
@@ -14,19 +14,22 @@ window.onscroll = () =>{
 
 // Get the modal
 var modalL = document.getElementById('id01');
-
+var modalR = document.getElementById('id02');
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modalL) {
         modalL.style.display = "none";
     }
-}
-
-var modalR = document.getElementById('id02');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modalR) {
+    else if (event.target == modalR) {
         modalR.style.display = "none";
     }
+}
+
+function reg(){
+    modalR.style.display='block';
+    modalL.style.display='none';
+}
+function log(){
+    modalR.style.display='none';
+    modalL.style.display='block';
 }
