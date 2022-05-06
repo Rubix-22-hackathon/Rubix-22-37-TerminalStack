177c5154c8a942d3613cfcd356fe9ea095515c44
Muskan Gupta <83824990+MuskanGupta2702@users.noreply.github.com>
2022-05-06 13:33:51 +0530

    password removed

--- src/utils/dbconnection.js
+++ src/utils/dbconnection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'PMmodi$$1',
+    password:'Your Password',
     database:'wecare'
   });
   connection.connect(function(error){
